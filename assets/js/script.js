2974552a57072e581203b02a9f28e0289a0f6a67
DaGitFella <davibezerra123457@gmail.com>
2024-09-06 12:18:49 -0300

    Questão 2 quase lá e bagunças no css

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,17 +2,39 @@ const pagesHeader = document.querySelector('.pages');
 const booksHeader = document.querySelector('.books');
 const greenColumn = document.querySelectorAll('.green');
 const blueColumn = document.querySelectorAll('.blue');
+const myForm = document.getElementById('create-text-fields');
+const myFormSelect = document.querySelector('#select');
+const textFieldContainer = document.querySelector('.text-fields-container');
 
 booksHeader.addEventListener('click', () => {
     blueColumn.forEach((item) => {
-        item.classList.toggle('active');
+        item.classList.add('active');
+        item.addEventListener('click', () => {
+            blueColumn.forEach((item) => {
+                item.classList.remove('active');
+            })
+        })
     })
 })
 
 pagesHeader.addEventListener('click', () => {
     greenColumn.forEach((item) => {
-        item.classList.toggle('active');
+        item.classList.add('active');
+        item.addEventListener('click', () => {
+            greenColumn.forEach((item) => {
+                item.classList.remove('active');
+            })
+        })
     })
+})
+
+myForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    let textField = document.createElement('input');
+    textField.type = 'text';
+    textField.setAttribute('class', 'form-control');
+
+    textFieldContainer.appendChild(textField);
 })
 
 var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
