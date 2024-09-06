8b1590fec9a8dbd6099869b882224599be7ddfb9
DaGitFella <davibezerra123457@gmail.com>
2024-09-06 14:32:16 -0300

    fim

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,9 @@ const blueColumn = document.querySelectorAll('.blue');
 const myForm = document.getElementById('create-text-fields');
 const myFormSelect = document.querySelector('#select');
 const textFieldContainer = document.querySelector('.text-fields-container');
+const zoomIn = document.querySelector('.bi-zoom-in');
+const zoomOut = document.querySelector('.bi-zoom-out');
+const images = document.querySelectorAll('.img-container img');
 
 booksHeader.addEventListener('click', () => {
     blueColumn.forEach((item) => {
@@ -30,14 +33,32 @@ pagesHeader.addEventListener('click', () => {
 
 myForm.addEventListener('submit', (event) => {
     event.preventDefault();
+    for(let i = 0; i < Number(myFormSelect.value); i++) {
     let textField = document.createElement('input');
     textField.type = 'text';
-    textField.setAttribute('class', 'form-control');
+    textField.classList.add('form-control', 'mt-5');
 
     textFieldContainer.appendChild(textField);
+
+    }
 })
+
+zoomIn.addEventListener('click', () => {
+    images.forEach(image => {
+        image.style.transform = 'scale(1.5)';
+    })
+})
+
+zoomOut.addEventListener('click', () => {
+    images.forEach(image => {
+        image.style.transform = 'scale(1)';
+    })
+})
+
+
 
 var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
     return new bootstrap.Tooltip(tooltipTriggerEl);
 });
+
